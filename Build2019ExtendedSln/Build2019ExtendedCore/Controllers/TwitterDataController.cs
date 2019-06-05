0cecf5719ada2e3f7003125ca1d2e313040aa45b
Eduardo Fonseca <efonsecab@gmail.com>
2019-06-05 17:46:28 -0600

    Adding sample call to Logic app

---
+++ Build2019ExtendedSln/Build2019ExtendedCore/Controllers/TwitterDataController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Build2019ExtendedCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Build2019ExtendedCore.Controllers
+{
+    public class TwitterDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public class RetrieveTwitterUserFollowersDataParameter
+        {
+            public string Username { get; set; }
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> GetTwitterUserFollowersData(string txtTwitterUserHandle)
+        {
+            RetrieveTwitterUserFollowersDataParameter parameter = new RetrieveTwitterUserFollowersDataParameter()
+            {
+                Username = txtTwitterUserHandle
+            };
+            var jsonParameter = Newtonsoft.Json.JsonConvert.SerializeObject(parameter);
+            RetrieveTwitterUserFollowersData result = null;
+            System.Net.Http.HttpClient httpClient = new System.Net.Http.HttpClient();
+            StringContent stringContent = new StringContent(jsonParameter);
+            stringContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue( "application/json");
+            var postResult = await httpClient.PostAsync("https://prod-00.westus.logic.azure.com:443/workflows/6ad354d7c7ab43edbb9aeecaa7526883/" +
+                "triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=kSK-iegBR0WguOblQqlfjrAXAO4DaDMDN3GLsZklU0k",
+                stringContent);
+            if (postResult.IsSuccessStatusCode)
+            {
+                var resultString = await postResult.Content.ReadAsStringAsync();
+                var convertResult = Newtonsoft.Json.JsonConvert.DeserializeObject<Class1[]>(resultString);
+                result = new RetrieveTwitterUserFollowersData()
+                {
+                    Property1 = convertResult
+                };
+            }
+            return View("GetTwitterUserFollowersData", result);
+        }
+    }
+}
---
+++ Build2019ExtendedSln/Build2019ExtendedCore/Models/RetrieveTwitterUserFollowersData.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Build2019ExtendedCore.Models
+{
+    public class RetrieveTwitterUserFollowersData
+    {
+        public Class1[] Property1 { get; set; }
+    }
+
+    public class Class1
+    {
+        public string TweetText { get; set; }
+        public string TweetId { get; set; }
+        public string CreatedAt { get; set; }
+        public DateTime CreatedAtIso { get; set; }
+        public int RetweetCount { get; set; }
+        public string TweetedBy { get; set; }
+        public string[] MediaUrls { get; set; }
+        public string TweetLanguageCode { get; set; }
+        public string TweetInReplyToUserId { get; set; }
+        public bool Favorited { get; set; }
+        public Usermention1[] UserMentions { get; set; }
+        public Originaltweet OriginalTweet { get; set; }
+        public Userdetails1 UserDetails { get; set; }
+    }
+
+    public class Originaltweet
+    {
+        public string TweetText { get; set; }
+        public string TweetId { get; set; }
+        public string CreatedAt { get; set; }
+        public DateTime CreatedAtIso { get; set; }
+        public int RetweetCount { get; set; }
+        public string TweetedBy { get; set; }
+        public string[] MediaUrls { get; set; }
+        public string TweetLanguageCode { get; set; }
+        public string TweetInReplyToUserId { get; set; }
+        public bool Favorited { get; set; }
+        public Usermention[] UserMentions { get; set; }
+        public Userdetails UserDetails { get; set; }
+    }
+
+    public class Userdetails
+    {
+        public string FullName { get; set; }
+        public string Location { get; set; }
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public int FollowersCount { get; set; }
+        public string Description { get; set; }
+        public int StatusesCount { get; set; }
+        public int FriendsCount { get; set; }
+        public int FavouritesCount { get; set; }
+        public string ProfileImageUrl { get; set; }
+    }
+
+    public class Usermention
+    {
+        public long Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+    }
+
+    public class Userdetails1
+    {
+        public string FullName { get; set; }
+        public string Location { get; set; }
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public int FollowersCount { get; set; }
+        public string Description { get; set; }
+        public int StatusesCount { get; set; }
+        public int FriendsCount { get; set; }
+        public int FavouritesCount { get; set; }
+        public string ProfileImageUrl { get; set; }
+    }
+
+    public class Usermention1
+    {
+        public long Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+    }
+
+}
